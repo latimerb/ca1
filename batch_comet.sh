81d75e834e07364a2911df7fada4d5583cd50a01
Ben  Latimer <pcp0129@login1.cm.cluster>
2018-05-23 20:21:20 +0200

    comet batch

--- batch_comet.sh
+++ batch_comet.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
-#SBATCH --output=ca1%j.out
-#SBATCH --partition=compute
-#SBATCH --qos=nsg
-#SBATCH --job-name=ca1
-#SBATCH -t 00:60:00
+#!/bin/bash
 
 #SBATCH --nodes=10
 #SBATCH --ntasks-per-node=20
+#SBATCH --job-name=ca1
+#SBATCH --output=ca1%j.out
+#SBATCH --time 0-00:30
 
 module load gsl
+
+echo "Starting model at $(date)"
 
 ibrun --npernode=24 nrniv -mpi main.hoc
