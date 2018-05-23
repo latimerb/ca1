f8c11375a0dcaa5c2d9b020e5aca53314df00b8e
Ben  Latimer <pcp0129@login2.cm.cluster>
2018-05-23 20:05:28 +0200

    comet batch

--- batch_comet.sh
+++ batch_comet.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH --output=ca1%j.out
-#SBATCH --partition=compute           # submit to the 'large' queue for jobs > 256 nodes
+#SBATCH --partition=compute
 #SBATCH --qos=nsg
 #SBATCH --job-name=ca1
-#SBATCH -t 00:60:00         # Run time (hh:mm:ss) - 1.5 hours
+#SBATCH -t 00:60:00
 
-#SBATCH --nodes=10              # Total number of nodes requested (16 cores/node)
-#SBATCH --ntasks-per-node=20            # Total number of mpi tasks requested
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=20
 
 module load gsl
 
-ibrun --npernode=24 /projects/ps-nsg/home/nsguser/applications/neuron7.4/installdir/x86_64/bin/nrniv  -mpi main.hoc
+ibrun --npernode=24 nrniv -mpi main.hoc
