7410ca8660c8a7d841132d4ebd2535e709da908c
Benjamin Latimer <latimerb@lewis4-r710-login-node223.cobalt.lan>
2018-05-23 10:49:26 -0500

    Problem with system() call in main

--- batch_lewis.sh
+++ batch_lewis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH -p Lewis,RC
-#SBATCH -N 5
+#SBATCH -N 1
 #SBATCH -A rc
-#SBATCH -n 100
+#SBATCH -n 10
 #SBATCH --qos=normal
-#SBATCH --job-name=Bigmodel
-#SBATCH --output=bigmodel%j.out
+#SBATCH --job-name=ca1
+#SBATCH --output=ca1%j.out
 #SBATCH --licenses=matlab:1
 #SBATCH --time 0-00:30
 
@@ -15,7 +15,7 @@ module load intel/intel-2016-update2
 module load nrn/nrn-mpi-7.4
 module load openmpi/openmpi-2.0.0
 
-
+module list
 echo "Starting model at $(date)"
 
 #srun -N 1 -n 1 -c 1 --mem 11G matlab -nodesktop -nosplash -nodisplay -r "run('./input/connectivity.m');exit"
